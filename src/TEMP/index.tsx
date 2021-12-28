05b070e4493493724321794324e8b87192752294
qsoft <qiusoft613@gmail.com>
2021-12-28 20:54:16 +0800

    chore: tsx temp

---
+++ src/TEMP/index.tsx
@@ -0,0 +1,12 @@
+import { ref, defineComponent } from "vue"
+import style from "./s.module.less"
+
+/** 组件 */
+const TEMP = defineComponent({
+    name: "TEMP",
+    setup() {
+        return () => <div></div>
+    },
+})
+
+export default TEMP
